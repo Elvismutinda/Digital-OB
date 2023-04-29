fb88bd000d14fb3f56288472cd26ab3974648b1b
Elvocool <elvocool@gmail.com>
2023-04-29 13:34:55 +0300

    reports

--- admin/adminpage.php
+++ admin/adminpage.php
@@ -12,34 +12,6 @@ include('adminmenu.php');
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Digital OB</title>
-   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-   <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(pieChart);
-
-      function pieChart(){
-
-         var data = google.visualization.arrayToDataTable([
-            ['crime_type', 'count'],
-            <?php
-            $chart_sql = "SELECT crime_type, COUNT(*) as count FROM cases GROUP BY crime_type"; 
-            $chart_result = mysqli_query($conn, $chart_sql);
-               
-            while($chart_row = mysqli_fetch_assoc($chart_result)){
-               echo"['".$chart_row['crime_type']."',".$chart_row['count']."],";
-            }
-            ?>
-         ]);
-
-         var options = {
-            title: 'Crime Reports Percentage',
-            is3D: true,
-         };
-
-         var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-         chart.draw(data, options);
-      }
-   </script>
 </head>
 <body>
 <form action="">
@@ -129,6 +101,34 @@ include('adminmenu.php');
                   <h2 class="charts-title">Case Analytics Reports</h2>
                   <div id="piechart_3d" style="width: 430px;height: 350px;"></div>
                </div>
+               <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+               <script type="text/javascript">
+                  google.charts.load('current', {'packages':['corechart']});
+                  google.charts.setOnLoadCallback(pieChart);
+
+                  function pieChart(){
+
+                     var data = google.visualization.arrayToDataTable([
+                        ['crime_type', 'count'],
+                        <?php
+                        $chart_sql = "SELECT crime_type, COUNT(*) as count FROM cases GROUP BY crime_type"; 
+                        $chart_result = mysqli_query($conn, $chart_sql);
+                           
+                        while($chart_row = mysqli_fetch_assoc($chart_result)){
+                           echo"['".$chart_row['crime_type']."',".$chart_row['count']."],";
+                        }
+                        ?>
+                     ]);
+
+                     var options = {
+                        title: 'Crime Reports Percentage',
+                        is3D: true,
+                     };
+
+                     var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+                     chart.draw(data, options);
+                  }
+               </script>
 
                <!-- <div class="charts-card">
                   <h2 class="charts-title">Yearly Crime Analytics Reports</h2>
--- incharge_officer/inchargepage.php
+++ incharge_officer/inchargepage.php
@@ -13,37 +13,6 @@ $station = $rows['station'];
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Digital OB</title>
-   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-   <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(pieChart);
-
-      function pieChart(){
-
-         var data = google.visualization.arrayToDataTable([
-            ['crime_type', 'count'],
-            <?php
-               $chart_sql = "SELECT crime_type, COUNT(*) as count FROM cases WHERE station='$station' GROUP BY crime_type"; 
-               $chart_result = mysqli_query($conn, $chart_sql);
-               
-               while($chart_row = mysqli_fetch_assoc($chart_result)){
-               echo"['".$chart_row['crime_type']."',".$chart_row['count']."],";
-               }
-            ?>
-         ]);
-
-         var options = {
-            title: 'Crime Reports Percentage',
-            is3D: true,
-         };
-
-         var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-         chart.draw(data, options);
-      }
-   </script>
-   <script type="text/javascript">
-
-   </script>
 </head>
 <body>
    <form action="">
@@ -133,11 +102,67 @@ $station = $rows['station'];
                   <h2 class="charts-title">Case Analytics Reports</h2>
                   <div id="piechart_3d" style="width: 430px;height: 350px;"></div>
                </div>
+               <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+               <script type="text/javascript">
+                  google.charts.load('current', {'packages':['corechart']});
+                  google.charts.setOnLoadCallback(pieChart);
 
-               <!-- <div class="charts-card">
-                  <h2 class="charts-title">Yearly Crime Analytics Reports</h2>
-                  <div id="barchart" style="width: 430px;height: 350px;"></div>
-               </div> -->
+                  function pieChart(){
+
+                     var data = google.visualization.arrayToDataTable([
+                        ['crime_type', 'count'],
+                        <?php
+                           $chart_sql = "SELECT crime_type, COUNT(*) as count FROM cases WHERE station='$station' GROUP BY crime_type"; 
+                           $chart_result = mysqli_query($conn, $chart_sql);
+                           
+                           while($chart_row = mysqli_fetch_assoc($chart_result)){
+                           echo"['".$chart_row['crime_type']."',".$chart_row['count']."],";
+                           }
+                        ?>
+                     ]);
+
+                     var options = {
+                        title: 'Crime Reports Percentage',
+                        is3D: true,
+                     };
+
+                     var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+                     chart.draw(data, options);
+                  }
+               </script>
+
+               <div class="charts-card">
+                  <h2 class="charts-title">Crime Analytics Reports</h2>
+                  <div id="columnchart" style="width: 430px;height: 350px;"></div>
+               </div>
+               <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+               <script type="text/javascript">
+                  google.charts.load('current', {'packages':['corechart']});
+                  google.charts.setOnLoadCallback(barChart);
+
+                  function barChart(){
+
+                     var data = google.visualization.arrayToDataTable([
+                        ['crime_type', 'count'],
+                        <?php
+                           $chart_sql = "SELECT crime_type, COUNT(*) as count FROM cases WHERE station='$station' GROUP BY crime_type"; 
+                           $chart_result = mysqli_query($conn, $chart_sql);
+                           
+                           while($chart_row = mysqli_fetch_assoc($chart_result)){
+                           echo"['".$chart_row['crime_type']."',".$chart_row['count']."],";
+                           }
+                        ?>
+                     ]);
+
+                     var options = {
+                        title: 'Crime Reports Count'
+                     };
+
+                     var chart = new google.visualization.ColumnChart(document.getElementById('columnchart'));
+                     chart.draw(data, options);
+                  }
+               </script>
+
             </div>
          </div>
       </div>
